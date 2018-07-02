8657158fc85edfdb995c4bc7bbfc0960fc6a4f6d
Amit Kumar Swami <amitiitm2009@gmail.com>
2018-07-02 15:49:02 +0800

    Update repo path for each

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ echo "Updating arcanist repos.."
 if [ -e "$PHP_DIR/libphutil" ]; then
     arc upgrade
 else
-    git clone git://github.com/facebook/libphutil.git "$PHP_DIR/libphutil"
-    git clone git://github.com/facebook/arcanist.git "$PHP_DIR/arcanist"
-    git clone git://github.com/facebook/phabricator.git "$PHP_DIR/phabricator"
+    git clone git://github.com/phacility/libphutil.git "$PHP_DIR/libphutil"
+    git clone git://github.com/phacility/arcanist.git "$PHP_DIR/arcanist"
+    git clone git://github.com/phacility/phabricator.git "$PHP_DIR/phabricator"
 fi
 
 # Register arc commands
